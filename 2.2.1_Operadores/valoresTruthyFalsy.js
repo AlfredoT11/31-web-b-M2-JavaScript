0e0f13ac89edbe142d0b1c198715f680bbdcbbc0
DoloresPGz <dolores.pgz@gmail.com>
2023-11-23 20:37:17 -0600

    valores Truthy y Falsy

---
+++ 2.2.1_Operadores/valoresTruthyFalsy.js
@@ -0,0 +1,15 @@
+//Ejemplos de valores Falsy
+console.log(Boolean(false)); //false
+console.log(Boolean(undefined)); //false
+console.log(Boolean(null));
+console.log(Boolean(0));
+console.log(Boolean(NaN));
+console.log(Boolean(''));
+
+//Ejemplos de valores Truthy
+console.log(Boolean(true));
+console.log(Boolean({}));
+console.log(Boolean([])); //arreglo
+console.log(Boolean(11));
+console.log(Boolean('0'));
+console.log(Boolean('false'));
