5408c2519eafb2ac6a7b018035ef9556ce69193b
DoloresPGz <dolores.pgz@gmail.com>
2023-11-23 20:40:01 -0600

    update de valores truthy y falsy

--- 2.2.1_Operadores/valoresTruthyFalsy.js
+++ 2.2.1_Operadores/valoresTruthyFalsy.js
@@ -3,13 +3,14 @@ console.log(Boolean(false)); //false
 console.log(Boolean(undefined)); //false
 console.log(Boolean(null));
 console.log(Boolean(0));
-console.log(Boolean(NaN));
+console.log(Boolean(NaN)); //Not a Number
 console.log(Boolean(''));
 
 //Ejemplos de valores Truthy
 console.log(Boolean(true));
 console.log(Boolean({}));
+console.log(Boolean({nombre: 'truthy'}));
 console.log(Boolean([])); //arreglo
 console.log(Boolean(11));
 console.log(Boolean('0'));
-console.log(Boolean('false'));
+console.log(Boolean("false"));
