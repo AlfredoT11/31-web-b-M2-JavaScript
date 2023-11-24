574f2daecd9dceb93df45943833b19d03a858c29
DoloresPGz <dolores.pgz@gmail.com>
2023-11-23 20:07:21 -0600

    operadores lógicos

---
+++ 2.2.1_Operadores/operadoresLogicos.js
@@ -0,0 +1,29 @@
+//Operadores lógicos &&, || y !
+let a = true;
+let b = false;
+let c = 5 < 10; //True
+
+console.log(a && b); //false
+console.log(a && c); //true
+console.log(a || b); //true
+console.log(!a) //false
+console.log(!b) //true 
+
+//------Cuando usamos el mismo operador para verificar varias condiciones---------
+//---- La evaluación se hace de izquierda a derecha
+let ejemplo1 = (5 > 3) && (8 < 10); //true
+let ejemplo2 =  (5 > 3) && (8 == 8) && (10 <= 1) //false
+let ejemplo3 =  (10 <= 1) && (8 == 8) && (5 > 3) //false
+
+//-------JERARQUÍA DE OPERADORES------
+let ejemplo4 = !true && false || true;
+//NOT se evalúa primero -> (false && false) || true
+//AND tiene mayor jerarquía que OR -> false || true
+//Al final evalúa los operadore OR -> true
+console.log(ejemplo4); //true
+
+let ejemplo5 = !true && (false || true);
+//Evalúa NOT -> false && (false || true)
+//Verfica los parértesis -> !true && true
+//Evalúa AND -> false
+console.log(ejemplo5); 
