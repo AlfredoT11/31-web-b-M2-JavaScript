43367f6e2d3c58f78e3736a5803de292f957ac3c
DoloresPGz <dolores.pgz@gmail.com>
2023-12-07 21:37:45 -0600

    lista de tareas funcional

--- 2.3.2_DOM/script.js
+++ 2.3.2_DOM/script.js
@@ -1,3 +1,5 @@
+/*
+//Código de ejemplos de teoría
 document.addEventListener("DOMContentLoaded", ()=>{
     const miTitulo = document.getElementById("miTitulo")
     const otroTitulo = document.getElementById("otroTitulo")
@@ -10,4 +12,33 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     alert("El DOM se ha cargado completamente")
     
-})
+})*/
+
+function agregarTarea() {
+    let input = document.getElementById("tareaInput")
+    //.trim() elimina los espacios vacíos antes y después de la cadena de texto
+    let textoTarea = input.value.trim()
+    
+    if(textoTarea){//El usuario debe ingresar por lo menos un caracter
+        //Creo mi nueva tarea
+        const listaTareas = document.getElementById("listaTareas")//Contender para poner mis tareas
+        const nuevaTarea = document.createElement("li")//Creo un li para agregar mi tarea en el html
+        
+        nuevaTarea.innerText = textoTarea //Coloco el texto de la tarea en el list item (li)
+        
+        //Crear mi botón de eliminar
+        const botonEliminar = document.createElement("button")
+        botonEliminar.innerText = "Eliminar"
+        botonEliminar.addEventListener("click", function () {
+            listaTareas.removeChild(nuevaTarea)//removeChild elimina un elemento del elemento padre
+        })
+
+        nuevaTarea.appendChild(botonEliminar)
+
+        listaTareas.appendChild(nuevaTarea) //agrega un elemento dentro de el elemento padre
+
+
+    }else{
+        //Mando un alert para que ponga texto
+    }
+}
